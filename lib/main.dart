9b20613dc146df2dce6745b7d63b54f0df6704f7
View-tum <tumsuwan5@gmail.com>
2025-01-12 16:11:16 +0700

    v.1 create project and set value for new project

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iot_sau_pm_project/views/splash_screen_ui.dart';
+
+void main() {
+  runApp(
+    IoTSauPMproject(),
+  );
+}
+
+class IoTSauPMproject extends StatefulWidget {
+  const IoTSauPMproject({super.key});
+
+  @override
+  State<IoTSauPMproject> createState() => _IoTSauPMprojectState();
+}
+
+class _IoTSauPMprojectState extends State<IoTSauPMproject> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenUi(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.kanitTextTheme(
+          Theme.of(context).textTheme,
+        )
+      )
+    );
+  }
+}
